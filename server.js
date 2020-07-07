2f9289fbbff634d9058f44053558252c220fdb70
ggsuccess <ggsuccess@naver.com>
2020-07-07 21:42:59 +0900

    package

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get();
